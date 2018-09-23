e92162a138de51842c0f0f76772c1134a0fcef0b
Alex Mathew <alexmathew003@gmail.com>
2018-09-23 15:29:34 +0530

    Reorganize sms service

--- noah_core/services/sms.py
+++ noah_core/services/sms.py
@@ -2,17 +2,69 @@
 At present, the app uses `Kaleyra <https://developers.kaleyra.com/>`_ for SMS integration. 
 
 To use a new provider - 
-1. Create the wrapper class in ``helpers/sms.py``, extending the ``SMSGateway`` class
+1. Create the wrapper class, extending the ``SMSGateway`` class
 2. Implement the ``send_sms`` method
 3. Create a new ``SMSService`` object as
 .. codeblock:: python
 
-   xyz = SMSService(provider=sms.Xyz())
+   xyz = SMSService(provider=Xyz())
 
 """
 
+from abc import ABCMeta, abstractmethod
 import os
-from ..helpers import sms
+import requests
+
+
+class SMSGateway(object):
+    """
+    Base class for all SMS gateway wrappers
+    """
+    __metaclass__ = ABCMeta
+
+
+    @abstractmethod
+    def send_sms(self, to_number, message_content, *args, **kwargs):
+        pass
+
+
+class Kaleyra(SMSGateway):
+    """
+    Wrapper around `Kaleyra <https://developers.kaleyra.com/>`_ for SMS integration
+
+    ``KALEYRA_API_KEY`` and ``KALEYRA_SENDER_ID`` should be set as env variables
+    """
+    API_KEY = os.getenv("KALEYRA_API_KEY") or None
+    SENDER_ID = os.getenv("KALEYRA_SENDER_ID") or None
+    METHOD = 'sms'
+    API_ENDPOINT = 'https://api-alerts.kaleyra.com/v4/'
+
+
+    def __init__(self):
+        """
+        """
+        pass
+
+
+    def send_sms(self, to_number, message_content, *args, **kwargs):
+        """
+        The API endpoint used here is described in the `Kaleyra documentation <https://developers.kaleyra.com/docs/send-an-sms>`_
+
+        :param to_number: Phone number to send the SMS to
+        :type username: str
+        :param message_content: Message to be sent
+        :type message_conten: str
+        :return: Response from Kaleyra API endpoint
+        """
+        querystring = {
+            'method': Kaleyra.METHOD,
+            'api_key': Kaleyra.API_KEY,
+            'sender': Kaleyra.SENDER_ID,
+            'to': str(to_number),
+            'message': message_content,
+        }
+        resp = requests.get(Kaleyra.API_ENDPOINT, params=querystring)
+        return resp.json()
 
 
 class SMSService:
@@ -29,21 +81,23 @@ class SMSService:
         self.provider = provider
 
 
-    def send(self, *, to_number=None, message=None, **kwargs):
+    def send(self, *, to_number=None, message_content=None, **kwargs):
         """
         Sends the message to the specified phone number. This is active only when the DEBUG env variable is false.
         The method takes the to_number and message (which should be keyword arguments), and other keyword arguments according to the provider.
 
         :param to_number: Phone number to send the SMS to
         :type username: str
-        :param message: Message to be sent
-        :type message: str
+        :param message_content: Message to be sent
+        :type message_content: str
         """
         if not (os.getenv('DEBUG', 'true') == 'true'):
-            resp = self.provider.send_sms(to_number, message, **kwargs)
+            if not (to_number and message_content):
+                raise Exception('`to_number` and `message_content` cannot be None')
+            resp = self.provider.send_sms(to_number, message_content, **kwargs)
             return resp
         else:
             print('SMS SENDING NOT AVAILABLE IN DEBUG MODE')
 
 
-kaleyra = SMSService(provider=sms.Kaleyra())
+kaleyra = SMSService(provider=Kaleyra())
